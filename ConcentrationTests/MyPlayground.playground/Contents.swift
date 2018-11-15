cde100e21d530034d5ddf7bc6ae19c3e107d323f
liteqd <liteqd@yahoo.com>
2018-11-15 07:53:46 -0800

    Ad d 3 m4a music files and have preliminary music running

---
+++ ConcentrationTests/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+
+}
+
+
